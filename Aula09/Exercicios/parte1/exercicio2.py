d8cba390550948d087e6b89077f64e8b38cb388c
Gregori Sabel <seriosabel57@gmail.com>
2020-08-28 15:47:50 -0300

    att

--- Aula09/Exercicios/parte1/exercicio2.py
+++ Aula09/Exercicios/parte1/exercicio2.py
@@ -5,6 +5,7 @@ Faça um programa que mostre a tabuada de 1, 2, 3 e 4
 """
 
 for x in range(4):
-    print("raboada do " + str(x))
+    print("Taboada do " + str(x))
     for y in range(11):
         print(str(y * x))
+
--- Aula09/Exercicios/parte1/exercicio3.py
+++ Aula09/Exercicios/parte1/exercicio3.py
@@ -7,8 +7,19 @@ Para 7 a 10 - Aprovado
 Para 5.5 a 7 - Recuperação 
 Para menor de 5.5 - Reprovado
 """
+
 total = 0
+
 for i in range(10):
-    nota = int(input("digite sua " + str(i+1) + "º nota"))
+    nota = int(input("digite sua " + str(i+1) + "º nota "))
     total += nota
-print("sua média é: "+str(total/i))
+
+media = total/10
+
+print("sua média é: "+str(media))
+if(media < 5.5):
+    print("Reprovado")
+elif(media < 7):
+    print("Recuperação")
+else: 
+    print("Aprovado")
